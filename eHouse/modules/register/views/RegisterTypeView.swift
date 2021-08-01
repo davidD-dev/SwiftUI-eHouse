9a4bd33b0ce30db92715c3d0847dfc3aceaf7614
davidD-dev <75249834+davidD-dev@users.noreply.github.com>
2021-08-01 16:05:44 +0200

    <Feat> : Add register view

---
+++ eHouse/modules/register/views/RegisterTypeView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterTypeView.swift
+//  eHouse
+//
+//  Created by David Deschamps on 01/08/2021.
+//
+
+import SwiftUI
+
+struct RegisterTypeView: View {
+    var body: some View {
+        Text("Registration")
+    }
+}
+
+struct RegisterTypeView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterTypeView()
+    }
+}
